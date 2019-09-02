f809ad6cdb122bb072fafee0917167cf367f7ed4
Kevin Martinez <kkevinmartinez7@gmail.com>
2019-09-02 15:57:07 -0600

    Union de componentes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigations';
 import { todos } from './todos.json';
+import TaskForm from './components/TaskFrom';
+
+
 console.log(todos);
 
 class App extends React.Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
       title: 'Aplicación de Tareas',
@@ -18,21 +21,21 @@ class App extends React.Component {
   }
 
   render() {
-    const todos = this.state.todos.map((todo, i) =>{
-      return(
+    const todos = this.state.todos.map((todo, i) => {
+      return (
         <div className="col-md-4">
-        <div className="card mt-5">
-          <div className="card-header">
-            <h3>{todo.title}</h3>
-            <span className="badge badge-pill badge-danger ml-2">
-              {todo.priority}
-            </span>
+          <div className="card mt-5">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{todo.description}</p>
+              <p className="badge badge-pill badge-primary ml-2">{todo.responsible}</p>
+            </div>
           </div>
-          <div className="card-body">
-            <p>{todo.description}</p>
-            <p className="badge badge-pill badge-primary ml-2">{todo.responsible}</p>
-          </div>
-        </div>
         </div>
       )
     })
@@ -40,13 +43,17 @@ class App extends React.Component {
     return (
       <div className="App">
         <Navigation titulo={this.state.title} />
-          <h1 className={this.state.vista}>{this.state.vista}</h1>
+        <h1 className={this.state.vista}>{this.state.vista}</h1>
 
-          <div className="container">
-            <div className="row mt-4">
-                {todos}
-            </div>
+        <div className="container">
+
+        <TaskForm/>
+
+          <div className="row mt-4">
+
+            {todos}
           </div>
+        </div>
 
         <img src={logo} className="App-logo" alt="logo" />
       </div>
--- src/components/TaskFrom.js
+++ src/components/TaskFrom.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class TaskForm extends Component{
-    constructor(){
+class TaskForm extends Component {
+    constructor() {
         super();
         this.state = {
             title: '',
@@ -9,21 +9,48 @@ class TaskForm extends Component{
             description: '',
             priority: 'low'
         };
+        this.handleInput = this.handleInput.bind(this);
+
     }
 
-    handleInput(e){
+    handleInput(e) {
         console.log(e.target.value, e.target.name);
-        const {value, name} = e.target;
+        const { value, name } = e.target;
         this.state({
             [name]: value
         })
         console.log(this.state)
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="card">
-                
+                <form className="card-body">
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="responsible"
+                            className="form-control"
+                            placeholder="Title"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="responsible"
+                            className="form-control"
+                            placeholder="Responsible"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="description"
+                            className="form-control"
+                            placeholder="Description"
+                        />
+                    </div>
+                </form>
             </div>
         )
     }
